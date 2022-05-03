1e98ee009c3fffcb262dcac8f958786ab184963f
mbuh divine <mbuhdivine_cho7@yahoo.com>
2022-05-03 20:03:03 +0100

    Added VRFConsumerBaseV2 to mediation contract

--- contracts/Mediation.sol
+++ contracts/Mediation.sol
@@ -1,6 +1,52 @@
 //SPDX-License-Identifier: MIT
 pragma Solidity 0.8.7;
 
-contract Mediation {
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+contract Mediation is VRFConsumerBaseV2 {
+    VRFCoordinatorV2Interface immutable i_COORDINATOR;
+    LinkTokenInterface immutable i_LINKTOKEN;
+    //Rinkeby coordinator, These test values are coming from https://docs.chain.link/docs/vrf-contracts/#configurations
+    address constant c_vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+    //subscription id, gotten from when you subscribe for LINK
+    uint64 immutable i_subscriptionId;
+    bytes32 constant c_keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+    uint32 constant c_callbackGasLimit = 100000;
+    uint16 constant c_requestConfirmations = 3;
+    uint32 constant c_numWords =  1;
+    uint256[] public s_randomNumbers;
+    uint256 public requestId;
+
+    address public s_owner;
+    constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
+        i_COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        s_owner = msg.sender;
+        s_subscriptionId = subscriptionId;
+    }
+
+
+
+
+
+
+    //Get random word.
+    function _requestRandomWords() internal {
+        // Will revert if subscription is not set and funded.
+        requestId = i_COORDINATOR.requestRandomWords(
+        c_keyHash,
+        s_subscriptionId,
+        c_requestConfirmations,
+        c_callbackGasLimit,
+        c_numWords
+        );
+    }
     
+    function fulfillRandomWords(
+        uint256, /* requestId */
+        uint256[] memory randomWords
+    ) internal override {
+        s_randomWords.push(randomWords[0]);
+    }
 }
